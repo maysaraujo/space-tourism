42e5cd0eb7de5234a7db065d43c883b3d532911e
Maysa Araujo <maysaaraujo25@gmail.com>
2024-10-07 21:45:51 -0300

    feat: creating home and its styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,23 @@ import Header from '../../components/Header/Header';
 
 const Home = () => {
   return (
-    <section className='bgHome'>
+    <section className='bgHome home'>
       <Header />
-      
+      <section className='container'>
+        <div className='text-container'>
+          <p>SO, YOU WANT TO TRAVEL TO</p>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className='explore'>
+          <p>EXPLORE</p>
+        </div>
+      </section>
     </section>
   );
 };
