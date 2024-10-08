dbd78db5be1c93cd79f853d2c8b9de43da2af363
Maysa Araujo <maysaaraujo25@gmail.com>
2024-10-07 21:44:47 -0300

    feat: creating destination page and its styles

---
+++ src/pages/Destination/Destination.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header';
+
+const Destination = () => {
+  return (
+    <section className='bgDestination destination'>
+      <Header />
+      <div className='container'>
+        <div className='pick-destination'>
+          <span>01</span>
+          <p>pick your destination</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Destination;
